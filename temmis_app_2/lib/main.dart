1b2c6ec2a79ed3b2c697e3fb2d66de6541582cd4
David <jhandry13@hotmail.com>
2020-02-07 17:10:10 -0500

    inicio con las media Query

--- temmis_app_2/lib/main.dart
+++ temmis_app_2/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:temmis_app_2/user/ui/screens/case_selection.dart';
-import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
---
+++ temmis_app_2/lib/resousers/size_conifg.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+
+class SizeConfig {
+  static MediaQueryData _mediaQueryData;
+  static double screenWidth;
+  static double screenHeight;
+  static double blockSizeHorizontal;
+  static double blockSizeVertical;
+}
