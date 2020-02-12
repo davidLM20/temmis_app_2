8d0c55caf21443e5d4535ce36fb51e36d3e6078b
David <jhandry13@hotmail.com>
2020-02-12 16:26:10 -0500

    subiendo cambios de boton

--- temmis_app_2/lib/thread/ui/screens/chatCase.dart
+++ temmis_app_2/lib/thread/ui/screens/chatCase.dart
@@ -5,6 +5,7 @@ import 'package:temmis_app_2/resousers/colors.dart';
 import 'package:temmis_app_2/thread/ui/widgets/ModalBottom.dart';
 //import 'package:temmis_app_2/user/ui/widgets/back_button.dart';
 import 'package:temmis_app_2/thread/ui/widgets/bubbleChat.dart';
+import 'package:temmis_app_2/user/ui/screens/alert_screen.dart';
 //import 'package:temmis_app_2/thread/model/case.dart';
 
 class ChatCase extends StatefulWidget {
@@ -16,11 +17,12 @@ class ChatCase extends StatefulWidget {
 }
 
 class _ChatCaseState extends State<ChatCase> {
-  _timer() {
+  //*****Medtodo de optencion de tiempo */
+  /*_timer() {
     Timer.periodic(Duration(seconds: 5), (timer) {
       print(DateTime.now());
     });
-  }
+  }*/
 
   List<BubbleChat> _message = <BubbleChat>[
     BubbleChat(
@@ -51,102 +53,13 @@ class _ChatCaseState extends State<ChatCase> {
 
   // funciones
 
-  final topo = Container(
-    alignment: Alignment.center,
-    margin: EdgeInsets.only(left: 10.0),
-    padding: EdgeInsets.only(top: 30.0, bottom: 30.0),
-    child: Row(
-      children: <Widget>[
-        Container(
-          padding: EdgeInsets.all(8.0),
-          child: BackButtonIcon(),
-        ),
-        Container(
-          width: 40.0,
-          height: 40.0,
-          decoration: BoxDecoration(
-            color: IndevColors.blue,
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-          ),
-          padding: EdgeInsets.all(9),
-        ),
-        Container(
-          //alignment: Alignment.bottomLeft,
-          padding: EdgeInsets.only(left: 10.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              RichText(
-                text: TextSpan(
-                  text: "Audiencia 1",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xDD000000),
-                      fontSize: 15.0,
-                      letterSpacing: 1,
-                      wordSpacing: 1),
-                ),
-              ),
-              //Spacer(flex: 1,),
-              RichText(
-                text: TextSpan(
-                  text: "Online...  ",
-                  style: TextStyle(
-                      color: Color(0xDD000000),
-                      fontSize: 15.0,
-                      letterSpacing: 1,
-                      wordSpacing: 1),
-                ),
-              ),
-            ],
-          ),
-        ), //texto
-      ],
-    ),
-  );
+  // final topo = ;
 
-  final boto = Container(
-    //padding: EdgeInsets.only(bottom: 10),
-    child: Row(
-      children: <Widget>[
-        Spacer(),
-        Container(
-          padding: EdgeInsets.all(4.0),
-          width: 55.0,
-          height: 55.0,
-          decoration: BoxDecoration(
-              color: Color(0xFFF1F1F1),
-              borderRadius: BorderRadius.all(Radius.circular(50))),
-          child: Icon(Icons.people, color: IndevColors.black),
-        ),
-        Spacer(),
-        Container(
-          padding: EdgeInsets.all(4.0),
-          width: 260.0,
-          height: 55.0,
-          decoration: BoxDecoration(
-              color: Color(0xFFF1F1F1),
-              borderRadius: BorderRadius.all(Radius.circular(20))),
-          child: ModalBottom1(),
-        ),
-        Spacer(),
-        Container(
-            padding: EdgeInsets.all(4.0),
-            width: 55.0,
-            height: 55.0,
-            decoration: BoxDecoration(
-                color: Color(0xFFF1F1F1),
-                borderRadius: BorderRadius.all(Radius.circular(15))),
-            child: Center(
-              child: Icon(Icons.send, color: IndevColors.black),
-            )),
-        Spacer(),
-      ],
-    ),
-  );
+  //final boto = ;
 
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      body:Container(
       color: IndevColors.gold,
       child: Container(
         margin: EdgeInsets.only(top: 30),
@@ -166,7 +79,64 @@ class _ChatCaseState extends State<ChatCase> {
                 padding: EdgeInsets.all(10.0),
                 child: SizedBox(
                   width: double.infinity,
-                  child: topo
+                  // parte superior de la pantalla inclullendo el
+                  child: Container(
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.only(left: 10.0),
+                    padding: EdgeInsets.only(top: 30.0, bottom: 30.0),
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.all(8.0),
+                          child: BackButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ),
+                        Container(
+                          width: 40.0,
+                          height: 40.0,
+                          decoration: BoxDecoration(
+                            color: IndevColors.blue,
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                          ),
+                          padding: EdgeInsets.all(9),
+                        ),
+                        Container(
+                          //alignment: Alignment.bottomLeft,
+                          padding: EdgeInsets.only(left: 10.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              RichText(
+                                text: TextSpan(
+                                  text: "Audiencia 1",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Color(0xDD000000),
+                                      fontSize: 15.0,
+                                      letterSpacing: 1,
+                                      wordSpacing: 1),
+                                ),
+                              ),
+                              //Spacer(flex: 1,),
+                              RichText(
+                                text: TextSpan(
+                                  text: "Online...  ",
+                                  style: TextStyle(
+                                      color: Color(0xDD000000),
+                                      fontSize: 15.0,
+                                      letterSpacing: 1,
+                                      wordSpacing: 1),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ), //texto
+                      ],
+                    ),
+                  )
                   //color: IndevColors.blue,
                   ,
                 ),
@@ -197,9 +167,10 @@ class _ChatCaseState extends State<ChatCase> {
                 //
               ),
             ), //burbujas
-            Container(
+            //**********metodo de obtencion de hora*/
+            /*Container(
               child: _timer(),
-            ),
+            ),*/
             Container(
               //alignment: AlignmentDirectional.bottomCenter,
               //padding: EdgeInsets.all(1.0),
@@ -207,13 +178,68 @@ class _ChatCaseState extends State<ChatCase> {
               alignment: Alignment.centerRight,
               child: Container(
                 padding: EdgeInsets.only(bottom: 7.0, left: 7.0, right: 7.0),
-                child: boto,
+                child: Container(
+                  //padding: EdgeInsets.only(bottom: 10),
+                  child: Row(
+                    children: <Widget>[
+                      Spacer(),
+                      Container(
+                        padding: EdgeInsets.all(4.0),
+                        width: 55.0,
+                        height: 55.0,
+                        decoration: BoxDecoration(
+                            color: Color(0xFFF1F1F1),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50))),
+                        child: Icon(Icons.people, color: IndevColors.black),
+                      ),
+                      Spacer(),
+                      Container(
+                        padding: EdgeInsets.all(4.0),
+                        width: 260.0,
+                        height: 55.0,
+                        decoration: BoxDecoration(
+                            color: Color(0xFFF1F1F1),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20))),
+                        child: ModalBottom1(),
+                      ),
+                      Spacer(),
+                      Container(
+                          padding: EdgeInsets.all(4.0),
+                          width: 55.0,
+                          height: 55.0,
+                          decoration: BoxDecoration(
+                              color: Color(0xFFF1F1F1),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(15))),
+                          child: GestureDetector(
+                              onTap: () {
+                                Navigator.of(context)
+                                    .push(MaterialPageRoute(builder: (context) {
+                                  return Overlay(
+                                    initialEntries: [
+                                      OverlayEntry(builder: (context) {
+                                        return AlertScreen();
+                                      }),
+                                    ],
+                                  );
+                                }));
+                              },
+                              child: Center(
+                                child:
+                                    Icon(Icons.send, color: IndevColors.black),
+                              ))),
+                      Spacer(),
+                    ],
+                  ),
+                ),
               ),
               //child: Container(child: Text('hola'),)
             ),
           ],
         ),
       ),
-    );
+    ));
   }
 }
--- temmis_app_2/lib/user/ui/widgets/role_card.dart
+++ temmis_app_2/lib/user/ui/widgets/role_card.dart
@@ -6,91 +6,82 @@ import 'package:temmis_app_2/thread/ui/screens/chatCase.dart';
 import 'package:temmis_app_2/user/ui/screens/role_screen.dart';
 
 class RoleCard extends StatelessWidget {
-
   String txt_role;
   String txt_description;
   String funtion_rol;
   String rol_icon;
 
+  RoleCard(
+      this.txt_role, this.txt_description, this.funtion_rol, this.rol_icon);
 
-  RoleCard(this.txt_role, this.txt_description, this.funtion_rol, this.rol_icon);
- 
   @override
   Widget build(BuildContext context) {
-    
     SizeConfig().init(context);
 
     final avatar = Container(
-      height: SizeConfig.blockSizeVertical*7,
-      width: SizeConfig.blockSizeHorizontal*16,
+      height: SizeConfig.blockSizeVertical * 7,
+      width: SizeConfig.blockSizeHorizontal * 16,
       margin: EdgeInsets.only(right: 20.0, left: 20.0),
       /*child: Icon(Icons.ac_unit,
         color: Colors.white,
         size: SizeConfig.blockSizeVertical*5,
       ),*/
       child: Container(
-        padding: EdgeInsets.all(SizeConfig.blockSizeHorizontal*1.5),
+        padding: EdgeInsets.all(SizeConfig.blockSizeHorizontal * 1.5),
         child: _selectIcons(rol_icon, color: _selectColor(funtion_rol)),
       ),
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(15.0)),
-        color: _selectColor(funtion_rol)
-      ),
+          borderRadius: BorderRadius.all(Radius.circular(15.0)),
+          color: _selectColor(funtion_rol)),
     );
 
     final name_role = Container(
-      child: Text(txt_role,
+      child: Text(
+        txt_role,
         style: TextStyle(
-          fontSize: SizeConfig.blockSizeVertical*3.2,
-          fontFamily: 'Schyler'
-        ),
+            fontSize: SizeConfig.blockSizeVertical * 3.2,
+            fontFamily: 'Schyler'),
       ),
     );
 
     final description_role = Container(
-
-      child:  Text(txt_description,
+      child: Text(
+        txt_description,
         style: TextStyle(
-          fontFamily: 'Schyler',
-          fontSize: SizeConfig.blockSizeVertical*2
-        ),
+            fontFamily: 'Schyler', fontSize: SizeConfig.blockSizeVertical * 2),
       ),
     );
 
-    final text_role =Container(
-      margin: EdgeInsets.only(top: SizeConfig.blockSizeVertical*2.5),
-      child: Column(children: <Widget>[
-        name_role,
-        description_role
-      ],),
+    final text_role = Container(
+      margin: EdgeInsets.only(top: SizeConfig.blockSizeVertical * 2.5),
+      child: Column(
+        children: <Widget>[name_role, description_role],
+      ),
     );
 
-    final card_rol =Container(
-      height: SizeConfig.blockSizeVertical*12,
+    final card_rol = Container(
+      height: SizeConfig.blockSizeVertical * 12,
       width: 320.0,
-      margin: EdgeInsets.only(left: 20.0, right: 20.0, top: SizeConfig.blockSizeVertical*3),
+      margin: EdgeInsets.only(
+          left: 20.0, right: 20.0, top: SizeConfig.blockSizeVertical * 3),
       decoration: BoxDecoration(
-        border: Border.all(color: Colors.black12, width: 2.0),
-        borderRadius: BorderRadius.circular(SizeConfig.blockSizeVertical*2)
-      ),
+          border: Border.all(color: Colors.black12, width: 2.0),
+          borderRadius:
+              BorderRadius.circular(SizeConfig.blockSizeVertical * 2)),
       child: Row(
-        children: <Widget>[
-          avatar,
-          text_role
-        ],
+        children: <Widget>[avatar, text_role],
       ),
     );
 
     return InkWell(
-      child: card_rol,
-      onTap: () {
+        child: card_rol,
+        onTap: () {
           Navigator.push(
               context, MaterialPageRoute(builder: (context) => ChatCase()));
-        },
-    );
+        });
   }
 
-  Color _selectColor(String rol){
+  Color _selectColor(String rol) {
     var color;
     switch (rol.toLowerCase()) {
       case "1":
@@ -101,26 +92,27 @@ class RoleCard extends StatelessWidget {
         break;
       case "3":
         color = IndevColors.blue;
-        break;    
+        break;
     }
 
     return color;
   }
 
-  SvgPicture _selectIcons(String iconRol, {Color color}){
+  SvgPicture _selectIcons(String iconRol, {Color color}) {
     var icon;
     switch (iconRol.toLowerCase()) {
       case "4":
-        icon = SvgPicture.asset("assets/img/juez_color.svg",);
+        icon = SvgPicture.asset(
+          "assets/img/juez_color.svg",
+        );
         break;
       case "5":
         icon = SvgPicture.asset("assets/img/fiscalia_color.svg");
         break;
       case "6":
         icon = SvgPicture.asset("assets/img/abogado_color.svg");
-        break;    
+        break;
     }
     return icon;
   }
-
 }
