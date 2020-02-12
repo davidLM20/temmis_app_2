777f26382b614fbbd6446b29c5261cf153c6097a
Jorge Alcivar Hurtado Duarte <jahurtado11@utpl.edu.ec>
2020-02-11 23:38:54 -0500

    feat(): add model and repository local

---
+++ temmis_app_2/lib/thread/model/route.dart
@@ -0,0 +1,113 @@
+// To parse this JSON data, do
+//
+//     final route = routeFromJson(jsonString);
+
+import 'dart:convert';
+
+Route routeFromJson(String str) => Route.fromJson(json.decode(str));
+
+String routeToJson(Route data) => json.encode(data.toJson());
+
+class Route {
+    String description;
+    List<RouteElement> route;
+
+    Route({
+        this.description,
+        this.route,
+    });
+
+    factory Route.fromJson(Map<String, dynamic> json) => Route(
+        description: json["description"],
+        route: List<RouteElement>.from(json["route"].map((x) => RouteElement.fromJson(x))),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "description": description,
+        "route": List<dynamic>.from(route.map((x) => x.toJson())),
+    };
+}
+
+class RouteElement {
+    String role;
+    List<Event> event;
+
+    RouteElement({
+        this.role,
+        this.event,
+    });
+
+    factory RouteElement.fromJson(Map<String, dynamic> json) => RouteElement(
+        role: json["role"],
+        event: List<Event>.from(json["event"].map((x) => Event.fromJson(x))),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "role": role,
+        "event": List<dynamic>.from(event.map((x) => x.toJson())),
+    };
+}
+
+class Event {
+    int id;
+    String text;
+    String role;
+    int next;
+    List<Option> option;
+    String description;
+    int scoreTotal;
+
+    Event({
+        this.id,
+        this.text,
+        this.role,
+        this.next,
+        this.option,
+        this.description,
+        this.scoreTotal,
+    });
+
+    factory Event.fromJson(Map<String, dynamic> json) => Event(
+        id: json["id"],
+        text: json["text"] == null ? null : json["text"],
+        role: json["role"] == null ? null : json["role"],
+        next: json["next"] == null ? null : json["next"],
+        option: json["option"] == null ? null : List<Option>.from(json["option"].map((x) => Option.fromJson(x))),
+        description: json["description"] == null ? null : json["description"],
+        scoreTotal: json["score_total"] == null ? null : json["score_total"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "text": text == null ? null : text,
+        "role": role == null ? null : role,
+        "next": next == null ? null : next,
+        "option": option == null ? null : List<dynamic>.from(option.map((x) => x.toJson())),
+        "description": description == null ? null : description,
+        "score_total": scoreTotal == null ? null : scoreTotal,
+    };
+}
+
+class Option {
+    String text;
+    int score;
+    int next;
+
+    Option({
+        this.text,
+        this.score,
+        this.next,
+    });
+
+    factory Option.fromJson(Map<String, dynamic> json) => Option(
+        text: json["text"],
+        score: json["score"],
+        next: json["next"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "text": text,
+        "score": score,
+        "next": next,
+    };
+}
